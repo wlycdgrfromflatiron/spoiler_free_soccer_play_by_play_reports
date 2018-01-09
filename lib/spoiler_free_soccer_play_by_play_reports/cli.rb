6740ec8ed3b2c3e63f673f503cee201e74f7dc28
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-08 22:25:29 -0500

    Stubbed out main CLI loop

--- lib/spoiler_free_soccer_play_by_play_reports/cli.rb
+++ lib/spoiler_free_soccer_play_by_play_reports/cli.rb
@@ -3,6 +3,7 @@ module SpoilerFreeSoccerPlayByPlayReports
         def self.start
             self.welcome
             self.controls
+            self.prompt_loop
         end
 
         def self.welcome
@@ -20,6 +21,46 @@ module SpoilerFreeSoccerPlayByPlayReports
             puts "[report list index]: View the corresponding report"
             puts "'controls', 'help': See these instructions"
             puts "'exit', 'quit': Quit the program"
+            puts ""
+        end
+
+        def self.prompt_loop
+            input = nil
+            while ('exit' != input && 'quit' != input)
+                puts 'Enter command: '
+                
+                input = gets.strip.downcase
+
+                if input.to_i > 0
+                    self.report(input.to_i)
+                else
+                    case input
+                    when 'chelsea', 'arsenal', 'city', 'all'
+                        self.report_list(input)
+                    when 'controls', 'help'
+                        self.controls
+                    else 
+                        self.invalid_input
+                    end
+                end
+            end
+        end
+
+        def self.report(report_index)
+            puts ""
+            puts "CLI.report called with report_index: #{report_index}"
+            puts ""
+        end
+
+        def self.report_list(team_name)
+            puts ""
+            puts "CLI.report_list called with team_name: #{team_name}"
+            puts ""
+        end
+
+        def self.invalid_input
+            puts ""
+            puts "CLI.invalid_input called"
             puts ""
         end
     end
