79e2d5eceaea5b64870c3e654bd9a7fa37193b65
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-08 21:59:09 -0500

    Added CLI.controls method that lists the interface commands

--- lib/spoiler_free_soccer_play_by_play_reports/cli.rb
+++ lib/spoiler_free_soccer_play_by_play_reports/cli.rb
@@ -2,12 +2,21 @@ module SpoilerFreeSoccerPlayByPlayReports
     class CLI
         def self.start
             self.welcome
+            self.controls
         end
 
         def self.welcome
             puts "" 
             puts "~ Welcome Spoiler Free Soccer Play by Play Reports ~"
             puts "(data provided by Sportsmole)"
+        end
+
+        def self.controls
+            puts "Controls:"
+            puts "[club's name]: See reports for that EPL club"
+            puts "'all': See reports for all EPL clubs"
+            puts "'controls', 'help': See these instructions"
+            puts "'exit', 'quit': Quit the program"
         end
     end
 end
