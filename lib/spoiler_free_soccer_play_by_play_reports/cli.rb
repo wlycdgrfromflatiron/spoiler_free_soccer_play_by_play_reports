ed86d2b1a8708dd454c5845d1e8b63130660d48e
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-08 22:03:30 -0500

    Improved welcome and controls printout formatting; added controls listing for viewing report details

--- lib/spoiler_free_soccer_play_by_play_reports/cli.rb
+++ lib/spoiler_free_soccer_play_by_play_reports/cli.rb
@@ -9,14 +9,18 @@ module SpoilerFreeSoccerPlayByPlayReports
             puts "" 
             puts "~ Welcome Spoiler Free Soccer Play by Play Reports ~"
             puts "(data provided by Sportsmole)"
+            puts "" 
         end
 
         def self.controls
+            puts ""
             puts "Controls:"
-            puts "[club's name]: See reports for that EPL club"
-            puts "'all': See reports for all EPL clubs"
+            puts "[club's name]: List avialable reports for that EPL club"
+            puts "'all': List available reports for all EPL clubs"
+            puts "[report list index]: View the corresponding report"
             puts "'controls', 'help': See these instructions"
             puts "'exit', 'quit': Quit the program"
+            puts ""
         end
     end
 end
