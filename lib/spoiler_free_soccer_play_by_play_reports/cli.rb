f8491e5f327a8769d86edfb0cd23b24b7fe44f0a
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-10 23:37:32 -0500

    Added separate input handling loop after a report list has been printed

--- lib/spoiler_free_soccer_play_by_play_reports/cli.rb
+++ lib/spoiler_free_soccer_play_by_play_reports/cli.rb
@@ -1,7 +1,8 @@
 module SpoilerFreeSoccerPlayByPlayReports
     class CLI
         @@just_printed_report_list = false
-        @@size_of_most_recently_printed_report_list = -1
+        @@report_list_size = -1
+        @@report_list_filter = "all"
 
         def self.start
             self.welcome
@@ -23,30 +24,35 @@ module SpoilerFreeSoccerPlayByPlayReports
             puts "All:          List all available reports."
             puts "[team name]:  List all available reports involving [team name]."
             puts "[report #]:   View the corresponding report (available after printing a report list)."
+            puts "[Spacebar]:   Show next report item (available on report detail screen)."
             puts "Help:         See these instructions."
-            puts "Exit, 'Quit': Quit the program."
+            puts "Exit, 'Quit': Quit back to main menu. If in main menu, quit."
             puts ""
-
-            @@just_printed_report_list = false
         end
 
         def self.main_loop
             input = ""
             while (!input.match(/^e(xit)?\s*$/))
+                @@just_printed_report_list = false
+
                 print "All | [team name] |  Help  |  Exit: "
                 
                 input = gets.strip.downcase
 
-                    case input
-                    when /^a(ll)?\s*$/
-                        self.report_list('all')
-                    when /^h(elp)?\s*$/
-                        self.controls
-                    when /^e(xit)?\s*$/, "", /^\s*$/
-                        # do nothing
-                    else
-                        self.report_list(input)
-                    end
+                case input
+                when /^a(ll)?\s*$/
+                    self.report_list('all')
+                when /^h(elp)?\s*$/
+                    self.controls
+                when /^e(xit)?\s*$/, "", /^\s*$/
+                    # do nothing
+                else
+                    self.report_list(input)
+                end
+
+                if @@just_printed_report_list && @@report_list_size > 0
+                    self.report_list_loop
+                end
             end
         end
 
@@ -94,12 +100,39 @@ module SpoilerFreeSoccerPlayByPlayReports
             puts ""
             
             reports = Report.list(team_name)
+
             reports.each.with_index(1) do |report, index|
                 puts "#{index}. #{report.team1} vs. #{report.team2}"
             end
 
-            @@size_of_most_recently_printed_report_list = reports.size
+            @@report_list_size = reports.size
+            @@report_list_filter = team_name
             @@just_printed_report_list = true
+        end
+
+        def self.report_list_loop
+            input = ""
+            while (!input.match(/^e(xit)?\s*$/))
+                print "[report #] | Help  |  Exit: "
+
+                input = gets.strip.downcase
+
+                if input.to_i > 0
+                    if input.to_i > @@report_list_size
+                        puts "Invalid index. Please try again"
+                    else
+                        self.report(input.to_i)
+                        self.report_list(@@report_list_filter)
+                    end
+                else
+                    case input
+                    when /^h(elp)?\s*$/
+                        self.controls
+                    when /^e(xit)?\s*$/, "", /^\s*$/
+                        # do nothing
+                    end
+                end
+            end
         end
 
         def self.invalid_input
