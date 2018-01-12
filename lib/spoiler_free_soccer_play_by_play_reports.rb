6b27f6a9d01b82e88bd112bcbe5d34045ab1319d
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-12 18:42:16 -0500

    Updated require in main lib file to look for correct wlycuteconsole filename

--- lib/spoiler_free_soccer_play_by_play_reports.rb
+++ lib/spoiler_free_soccer_play_by_play_reports.rb
@@ -1,4 +1,4 @@
-require "spoiler_free_soccer_play_by_play_reports/wlyformat"
+require "spoiler_free_soccer_play_by_play_reports/wlycuteconsole"
 require "spoiler_free_soccer_play_by_play_reports/version"
 require "spoiler_free_soccer_play_by_play_reports/cli"
 require "spoiler_free_soccer_play_by_play_reports/scraper"
