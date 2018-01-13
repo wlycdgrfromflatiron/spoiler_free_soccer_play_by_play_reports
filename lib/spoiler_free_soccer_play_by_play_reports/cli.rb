fa282469c7297719d41e3bce0e875c9d1599d32c
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-12 20:25:36 -0500

    Cleanup of CLI class. Removing redundant functions and variables.

--- lib/spoiler_free_soccer_play_by_play_reports/cli.rb
+++ lib/spoiler_free_soccer_play_by_play_reports/cli.rb
@@ -7,9 +7,9 @@ module SpoilerFreeSoccerPlayByPlayReports
         STATE_MATCHES_LIST = 2
         STATE_TEAMS_LIST = 3
         STATE_REPORT = 4
+        STATE_QUIT = 5
         @@state = nil
-
-        @@should_exit = false
+        @@changing_state = false
 
         @@matches_list = []
         @@matches_list_team_name = nil
@@ -22,50 +22,25 @@ module SpoilerFreeSoccerPlayByPlayReports
         PUTS_INDENT = 5
         PRINT_INDENT = 5
 
+        # ENTRY POINT
         def self.start
             set_default_puts_indent(PUTS_INDENT)
             set_default_print_indent(PRINT_INDENT)
 
-            system "clear" or system "cls"
-
-            puts ""; self.welcome; puts ""
+            system "clear" or system "cls"; puts ""
+            self.welcome; puts ""
             puts_indented("Loading report list...")
             Report.list('all')
 
-            @@state = STATE_MAIN_MENU
-            @@previous_state = nil
+            self.state(STATE_MAIN_MENU)
             self.main_loop
 
-            self.goodbye
+            puts ""; puts_indented("Thanks for using this app. Goodbye!"); puts ""
         end
 
-        def self.welcome
-            puts_indented("~ SPOILER-FREE PLAY-BY-PLAY SOCCER MATCH REPORTS ~")
-            puts_indented("A service for reading live commentaries for completed soccer matches")
-            puts_indented("in chronological order and without spoilers.")
-            puts_indented("(data source: SPORTSMOLE.CO.UK)")
-        end
-
-        def self.main_menu_controls
-            puts_indented("MAIN MENU CONTROLS:")
-            puts_indented("(M)atches:        List all matches for which reports are available.")
-            puts_indented("(T)eams:          List all teams for which reports are available.")
-            puts_indented("[team name]:      List all available reports for [team name].")
-            puts_indented("(E)xit:           Exit the program.")
-        end
-
-        def self.error_message
-            puts_indented(@@error_message) if @error_message
-            @@error_message = nil
-        end
-
-        def self.state(new_state)
-            @@previous_state = @@state
-            @@state = new_state
-        end
-
+        # MAIN LOOP
         def self.main_loop
-            while !@@should_exit
+            while STATE_QUIT != @@state
                 case @@state
                 when STATE_MAIN_MENU
                     self.main_menu_loop
@@ -76,64 +51,58 @@ module SpoilerFreeSoccerPlayByPlayReports
                 when STATE_REPORT
                     self.report_loop
                 end
+                @@changing_state = false
             end
         end
 
+        # STATE HANDLING
+        def self.state(new_state)
+            @@previous_state = @@state
+            @@state = new_state
+            @@changing_state = true
+        end
+
+        # MAIN MENU LOOP AND ITS HELPER FUNCTIONS
         def self.main_menu_loop
-            in_this_state = true
             input = ""
 
-            while (in_this_state)
-                system "clear" or system "cls"
-
-                puts ""
-                self.welcome
-                puts ""
-                self.main_menu_controls
+            while (!@@changing_state)
+                system "clear" or system "cls"; puts ""
+                self.welcome; puts ""
+                puts_indented("MAIN MENU CONTROLS:")
+                puts_indented("(M)atches:        List all matches for which reports are available.")
+                puts_indented("(T)eams:          List all teams for which reports are available.")
+                puts_indented("[team name]:      List all available reports for [team name].")
+                puts_indented("(E)xit:           Exit the program.")
                 puts ""
                 self.error_message
             
-                print_indented("(M)atches | (T)eams | [team name] | (E)xit: ")
-                input = gets.strip
+                print_indented("(M)atches | (T)eams | [team name] | (E)xit: "); input = gets.strip
 
-                # User wants to quit
                 if input.match(/^e(xit)?\s*$/)
-                    @@should_exit = true
-                    in_this_state = false
+                    this.state(STATE_QUIT)
 
-                # User wants to see a list of all matches
                 elsif input.match(/^m(atches)?\s*?/)
                     @@matches_list = Report.matches
                     if !@@matches_list.empty?
                         self.state(STATE_MATCHES_LIST)
-                        in_this_state = false
                     else
                         @@error_message = "No matches are currently available :("
                     end
 
-                # User wants to see a list of all teams
                 elsif input.match(/^t(eams)?\s*?/)
                     @@teams_list = Report.teams
                     if !@@teams_list.empty?
                         self.state(STATE_TEAMS_LIST)
-                        in_this_state = false
                     else
                         @@error_message = "No reports are currently available for any teams :("
                     end
 
-                # Or, user is trying to enter a team name...probably... - 
-                # but since any old gibberish COULD be a team name that we forgot or didn't know about,
-                # let's not risk filtering out what could be a valid search - 
-                # let's just trust the user and see if there are any matching reports for whatever they entered.
-                # If user is trolling, we just won't get any results, no big deal.
-                # (Heh, no doubt there's some terrible security hole I'm leaving open...)
                 else
                     @@matches_list = Report.matches
                     if !@@matches_list.empty?
                         @@matches_list_team_name = input
-
                         self.state(STATE_MATCHES_LIST)
-                        in_this_state = false
                     else
                         @@error_message = "No matches are available for #{team_name} :("\
                             "\n...However, the parser is not the brightest."\
@@ -143,8 +112,8 @@ module SpoilerFreeSoccerPlayByPlayReports
             end
         end
 
+        # MATCHES LIST LOOP
         def self.matches_list_loop
-            in_this_state = true
             input = ""
 
             header = @@matches_list_team_name ? 
@@ -157,20 +126,13 @@ module SpoilerFreeSoccerPlayByPlayReports
             end
             match_list_string = column_print(match_strings)
 
-            while (in_this_state)
-                system "clear" or system "cls"
-
-                puts ""
-                puts_indented(header)
-                puts ""
-
-                puts match_list_string
-                puts ""
-
+            while (!@@changing_state)
+                system "clear" or system "cls"; puts ""
+                puts_indented(header); puts ""
+                puts match_list_string; puts ""
                 self.error_message
 
-                print_indented("[report #] | (B)ack | (E)xit: ")
-                input = gets.chomp
+                print_indented("[report #] | (B)ack | (E)xit: "); input = gets.chomp
 
                 # User is trying to select a report to view
                 if input.to_i > 0
@@ -178,26 +140,20 @@ module SpoilerFreeSoccerPlayByPlayReports
                         @@error_message = "Invalid report number! Please try again."
                     else
                         @@report_index = input.to_i
-
                         self.state(STATE_REPORT)
-                        in_this_state = false
                     end
                 else 
-                    in_this_state = self.handle_back_exit_and_misc(input)
+                    self.handle_back_exit_and_misc(input)
                 end
             end
         end
 
         def self.teams_list_loop
-            in_this_state = true
             input = ""
 
-            while (in_this_state)
-                system "clear" or system "cls"
-
-                puts ""
-                puts_indented("TEAMS THAT HAVE REPORTS AVAILABLE:")
-                puts ""
+            while (!@@changing_state)
+                system "clear" or system "cls"; puts ""
+                puts_indented("TEAMS THAT HAVE REPORTS AVAILABLE:"); puts ""
 
                 @@teams_list.each.with_index(1) do |team_name, index|
                     puts_indented("#{index}. #{team_name}")
@@ -218,10 +174,9 @@ module SpoilerFreeSoccerPlayByPlayReports
                         @@matches_list_team_name = team_name
 
                         self.state(STATE_MATCHES_LIST)
-                        in_this_state = false
                     end
                 else 
-                    in_this_state = self.handle_back_exit_and_misc(input, true, true, true)
+                    self.handle_back_exit_and_misc(input)
                 end
             end
         end
@@ -229,7 +184,14 @@ module SpoilerFreeSoccerPlayByPlayReports
         def self.report_loop
             report = Report.report(@@report_index)
             
-            self.report_preamble(report)
+            puts ""
+            puts_indented("MATCH REPORT")
+            puts_indented("#{report.team1} VS. #{report.team2}")
+            puts_indented("")
+            puts_indented("Author: #{report.byline.author}")
+            puts_indented("Filed: #{report.byline.filed}")
+            puts_indented("Updated: #{report.byline.updated}")
+            puts ""
 
             puts ""
             puts_indented("Controls:")
@@ -238,12 +200,10 @@ module SpoilerFreeSoccerPlayByPlayReports
             puts_indented("q:               Exit the program.")
             puts ""
 
-            in_this_state = true
             input = ""
-            while (in_this_state && !Report.done)
+            while (!@@changing_state && !Report.done)
                 input = STDIN.getch
 
-                # User wants to see the next blurb
                 if ' ' == input
                     blurb = Report.next_blurb
                     puts ""
@@ -251,7 +211,7 @@ module SpoilerFreeSoccerPlayByPlayReports
                     puts_indented("#{blurb.text}")
                     puts ""
                 else
-                    in_this_state = self.handle_back_exit_and_misc(input, true, true, false)
+                    self.handle_back_exit_and_misc(input, false)
                 end
             end
 
@@ -261,42 +221,28 @@ module SpoilerFreeSoccerPlayByPlayReports
             end
         end
 
-        def self.report_preamble(report)
-            puts ""
-            puts_indented("MATCH REPORT")
-            puts_indented("#{report.team1} VS. #{report.team2}")
-            puts_indented("")
-            puts_indented("Author: #{report.byline.author}")
-            puts_indented("Filed: #{report.byline.filed}")
-            puts_indented("Updated: #{report.byline.updated}")
-            puts ""
+        # PRINTER FUNCTIONS
+        def self.error_message
+            puts_indented(@@error_message) if @error_message
+            @@error_message = nil
         end
 
-        def self.goodbye
-            puts ""
-            puts_indented("Thanks for using this app. Goodbye!")
-            puts ""
+        def self.welcome
+            puts_indented("~ SPOILER-FREE PLAY-BY-PLAY SOCCER MATCH REPORTS ~")
+            puts_indented("A service for reading live commentaries for completed soccer matches")
+            puts_indented("in chronological order and without spoilers.")
+            puts_indented("(data source: SPORTSMOLE.CO.UK)")
         end
 
-        def self.handle_back_exit_and_misc(input, handle_back, handle_exit, handle_other)
-            in_this_state = true 
-
-            # User is trying to return to previous screen
-            if handle_back && input.match(/^b(ack)?\s*$/)
+        # INPUT HANDLING
+        def self.handle_back_exit_and_misc(input, handle_other=true)
+            if input.match(/^b(ack)?\s*$/)
                 self.state(@@previous_state)
-                in_this_state = false
-
-            # User is trying to exit the program
-            elsif handle_exit input.match(/^e(xit)?\s*$/)
-                @@should_exit = true
-                in_this_state = false
-
-            # User has entered some funsense
+            elsif input.match(/^e(xit)?\s*$/)
+                self.state(STATE_QUIT)
             elsif handle_other
                 @@error_message = "To make a selection, enter its number."
             end
-
-            in_this_state
         end
     end
 end
