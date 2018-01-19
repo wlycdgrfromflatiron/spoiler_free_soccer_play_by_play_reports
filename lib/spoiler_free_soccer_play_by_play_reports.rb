b22705f66106c643ae9c2f1b0ad83d0081249377
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-19 17:51:34 -0500

    Added explanatory comments to main lib file, the one that serves as the environment config. This seems to be a good way to verify my own understanding and to confirm that components serve a clear purpose.

--- lib/spoiler_free_soccer_play_by_play_reports.rb
+++ lib/spoiler_free_soccer_play_by_play_reports.rb
@@ -1,12 +1,12 @@
 require "spoiler_free_soccer_play_by_play_reports/version"
-require "spoiler_free_soccer_play_by_play_reports/formatter"
-require "spoiler_free_soccer_play_by_play_reports/printer"
-require "spoiler_free_soccer_play_by_play_reports/cli"
-require "spoiler_free_soccer_play_by_play_reports/scraper"
-require "spoiler_free_soccer_play_by_play_reports/report"
-require "spoiler_free_soccer_play_by_play_reports/blurb"
-require "nokogiri"
-require "open-uri"
-require "io/console"
+require "spoiler_free_soccer_play_by_play_reports/formatter" # custom string formatting
+require "spoiler_free_soccer_play_by_play_reports/printer" # console output
+require "spoiler_free_soccer_play_by_play_reports/cli" # command-line interface logic
+require "spoiler_free_soccer_play_by_play_reports/scraper" # retrieves and parses data source (originally Sportsmole)
+require "spoiler_free_soccer_play_by_play_reports/report" # data model, populated by scraper
+require "spoiler_free_soccer_play_by_play_reports/blurb" 
+require "nokogiri" # HTML parsing
+require "open-uri" # HTML retrieval
+require "io/console" # unbuffered input handling
 
-require "pry" if WLY_DEBUG
+require "pry" if WLY_DEBUG # interactive debugging
