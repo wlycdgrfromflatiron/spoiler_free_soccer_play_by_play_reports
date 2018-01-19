a4b0886abb8eac0d30968afa4548637aefd6e79c
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-18 20:20:42 -0500

    Added reqire for Formatter file

--- lib/spoiler_free_soccer_play_by_play_reports.rb
+++ lib/spoiler_free_soccer_play_by_play_reports.rb
@@ -1,4 +1,5 @@
 require "spoiler_free_soccer_play_by_play_reports/version"
+require "spoiler_free_soccer_play_by_play_reports/formatter"
 require "spoiler_free_soccer_play_by_play_reports/cli"
 require "spoiler_free_soccer_play_by_play_reports/scraper"
 require "spoiler_free_soccer_play_by_play_reports/report"
