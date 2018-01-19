acb6b4a089fc4370496e2dd77d73f89641aea66e
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-19 17:53:11 -0500

    Edited explanatory comments in main lib file

--- lib/spoiler_free_soccer_play_by_play_reports.rb
+++ lib/spoiler_free_soccer_play_by_play_reports.rb
@@ -2,11 +2,11 @@ require "spoiler_free_soccer_play_by_play_reports/version"
 require "spoiler_free_soccer_play_by_play_reports/formatter" # custom string formatting
 require "spoiler_free_soccer_play_by_play_reports/printer" # console output
 require "spoiler_free_soccer_play_by_play_reports/cli" # command-line interface logic
-require "spoiler_free_soccer_play_by_play_reports/scraper" # retrieves and parses data source (originally Sportsmole)
+require "spoiler_free_soccer_play_by_play_reports/scraper" # domain-specific data retrieval and parsing
 require "spoiler_free_soccer_play_by_play_reports/report" # data model, populated by scraper
 require "spoiler_free_soccer_play_by_play_reports/blurb" 
-require "nokogiri" # HTML parsing
-require "open-uri" # HTML retrieval
+require "nokogiri" # HTML parsing support library
+require "open-uri" # HTML retrieval support library
 require "io/console" # unbuffered input handling
 
 require "pry" if WLY_DEBUG # interactive debugging
