d9fdbae41cb5f5ba176e59601c5ae7311e547284
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-22 00:23:30 -0500

    Added column width validation and adjustment in Formatter

--- lib/spoiler_free_soccer_play_by_play_reports/formatter.rb
+++ lib/spoiler_free_soccer_play_by_play_reports/formatter.rb
@@ -9,12 +9,15 @@ module SpoilerFreeSoccerPlayByPlayReports
         ###################
         COLUMN_WIDTH = 50
         INDENT = "     "
+        MINIMUM_COLUMNN_GAP = 2
 
 
         ########################
         # PUBLIC CLASS METHODS #
         ########################
         def self.columnize(string_array, column_width = COLUMN_WIDTH)
+            column_width = validate_column_width(string_array, column_width)
+
             odd_end = string_array.size.even? ? nil : string_array.pop
 
             columnized_string = ""
@@ -44,7 +47,18 @@ module SpoilerFreeSoccerPlayByPlayReports
             row << "\n"
         end
 
-        private_class_method :row
+        def self.validate_column_width(string_array, column_width)
+            half_way = string_array.size.even ? string_array.size / 2 : (string_array.size + 1) / 2
+
+            for i in 0...half_way
+                min_required_width = string_array[i].length + MINIMUM_COLUMN_GAP
+                column_width = min_required_width if min_required_width > column_width
+            end
+
+            column_width
+        end
+
+        private_class_method :row, :validate_column_width
 
 
     end # class Formatter
