3f71b4f4116c894cbf3c79118972ff3601feb525
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-22 20:52:12 -0500

    Removed duplicate declaration of WLY_DEBUG

--- lib/spoiler_free_soccer_play_by_play_reports.rb
+++ lib/spoiler_free_soccer_play_by_play_reports.rb
@@ -8,5 +8,4 @@ require "nokogiri" # HTML parsing support library
 require "open-uri" # HTML retrieval support library
 require "io/console" # unbuffered input handling
 
-WLY_DEBUG = true
 require "pry" if WLY_DEBUG # interactive debugging
